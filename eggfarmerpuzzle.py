86f903a6e330f41bbee517a40dc77d2ac0038459
BNLXG <jorn.erik.jenssen@zoho.eu>
2025-03-28 00:36:00 +0100

    Add files via upload

---
+++ eggfarmerpuzzle.py
@@ -0,0 +1,25 @@
+# The Egg Farmer's Puzzle - www.101computing.net/the-egg-farmer-puzzle/
+
+# Can be solved with math or for-loops
+# This is the math solution
+
+print("How many eggs did you get today, Farmer?")
+eggs = int(input())
+
+cartons = 12
+
+cartons_math = eggs / 12
+cartons_needed = int(cartons_math)
+cartons_overflow = eggs - (cartons_needed * 12)
+
+boxes = 6
+
+if cartons_overflow > 6:
+    boxes_math = cartons_overflow / 6
+    boxes_needed = int(boxes_math)
+    box_overflow = cartons_overflow - (boxes_needed * 6)
+    print("You can bring: " + str(cartons_needed) + " cartons and " + str(boxes_needed) + " boxes, and eat " +
+          str(int(box_overflow)) + " for breakfast!")
+
+else:
+    print("You can eat " + str(box_overflow) + "for breakfast!")
---
+++ eggsforfarmerpuzzle.py
@@ -0,0 +1,31 @@
+# The Egg Farmer's Puzzle - www.101computing.net/the-egg-farmer-puzzle/
+
+# Can be solved with math or for-loops
+# This is the loop solution
+
+print("How many eggs did you get today, Farmer?")
+eggs = int(input())
+
+carton_size = 12
+box_size = 6
+
+cartons_needed = 0
+boxes_needed = 0
+eggs_remaining = eggs
+
+for i in range(eggs // carton_size):
+    cartons_needed += 1
+    eggs_remaining -= carton_size
+
+if eggs_remaining > box_size:
+    for i in range(eggs_remaining // box_size):
+        boxes_needed += 1
+        eggs_remaining -= box_size
+
+if boxes_needed > 0:
+    print(
+        "You can bring: " + str(cartons_needed) + " cartons and " + str(boxes_needed) + " boxes, and eat " + str(eggs_remaining) +
+        " for breakfast!")
+else:
+    print(
+        "The Eggs fit perfectly in " + str(cartons_needed) + " cartons and " + str(boxes_needed) + " boxes and you can eat " + str(eggs_remaining) + " for breakfast!")
